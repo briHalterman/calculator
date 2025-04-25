a2a41d18d63edb8470e3ac7d7c0fc62207d206a0
Brittany Halterman <halterma@gmail.com>
2025-04-25 11:38:58 -0400

    Implement calculate button and logic
    - Adds handler for "=" button to capture second operand and call
    operate()
    - Extracts operandB using split after last space
    - Displays full equation in display before calculating
    - Uses data-operator attributes to separate logic from visual labels

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-let operandA;
-let operandB;
+// const operate = window.operate;
+
+let operandA = undefined;
+let operandB = undefined;
 let operator = "";
 let displayValue = "";
 
 const digitButtons = document.querySelectorAll(".digit");
 const operatorButtons = document.querySelectorAll(".operator");
+const calculateButton = document.querySelector(".equal-sign");
 
 const updateDisplay = () => {
   const display = document.getElementById("display");
@@ -25,11 +28,31 @@ operatorButtons.forEach((button) => {
   button.addEventListener("click", (event) => {
     if (operandA === undefined && displayValue !== "") {
       operandA = parseFloat(displayValue);
-      // console.log(operandA); // for manual testing
-      operator = event.target.textContent;
-      // console.log(operator); // for manual testing
-      displayValue = "";
+      console.log(operandA); // for manual testing
+      operator = event.target.dataset.operator;
+      console.log(operator); // for manual testing
+      displayValue += ` ${event.target.textContent} `;
       updateDisplay();
     }
   });
+});
+
+calculateButton.addEventListener("click", () => {
+  if (
+    operandA !== undefined &&
+    operator !== "" &&
+    displayValue !== ""
+  ) {
+    operandB = parseFloat(displayValue.split(" ").pop());
+    console.log(operandB); // for manual testing
+    const calculation = window.operate(operandA, operandB, operator);
+
+    displayValue = calculation.toString();
+    console.log(displayValue); // for manual testing
+    updateDisplay();
+
+    operandA = undefined;
+    operandB = undefined;
+    operator = "";
+  }
 });
